fbf7770cd443562947d9c51eb8689bf4de15e8dd
Andrew Knee <andrew.knee.j@gmail.com>
2018-06-13 14:29:19 -0400

    Update Economy_Simulator.java

--- Economy_Simulator/Economy_Simulator.java
+++ Economy_Simulator/Economy_Simulator.java
@@ -4,32 +4,44 @@ import java.util.Scanner;
 
 public class Economy_Simulator {
 
+	static Scanner scanner = new Scanner(System.in);
+	
+	static String growth, selection;
+	static int gdpA, gdpB, tradeValue, total;
+	static float percentOfExport, average, last;
+	
 	//Gets a random number from 0 to 9
 	public static int randomNumber() {
 		return ((int)((Math.random()) * 10));
 	}
 	
+	public static void economySimulation() {
+		System.out.println("Please input the GDP of the first country in dollars:");
+		gdpA = scanner.nextInt();
+		System.out.println("Please input the GDP of the second country in dollars:");
+		gdpB = scanner.nextInt();
+		System.out.println("Please input the value of the trade deal in dollars:");
+		tradeValue = scanner.nextInt();
+		System.out.println("Please input the total imports/exports of the country that is recieving the trade deal in dollars:");
+		total = scanner.nextInt();
+		
+		percentOfExport = tradeValue / total;
+		average = (gdpA + gdpB) / 2;
+		last = (average * percentOfExport) % randomNumber();
+	}
+	
+	public static void researchSimulation() {
+		
+	}
+	
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		String selection, growth;
-		int gdpA, gdpB, tradeValue, total;
-		float percentOfExport;
-		
 		System.out.println("Would you like the use Economy or Research Simulation? Y/N");
 		selection = scanner.nextLine();
-		if(selection == "Y") {
-			System.out.println("Do you want to calculate Growth from Trade or Building?");
-			growth = scanner.nextLine();
-			if(growth == "Trade") {
-				System.out.println("Please input the GDP of the country recieving the trade.");
-				gdpA = scanner.nextInt();
-				System.out.println("Please input the GDP of the country giving the trade.");
-				gdpB = scanner.nextInt();
-			}
+		if(selection.equals("Y")) {
+			economySimulation();
 		}
 		else {
-			
+			researchSimulation();
 		}
 	}
 

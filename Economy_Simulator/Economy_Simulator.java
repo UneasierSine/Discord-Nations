ed145459c546d58c27b5ebc689500df9b6f65bcc
Andrew Knee <andrew.knee.j@gmail.com>
2018-06-12 23:40:28 -0400

    Update Economy_Simulator.java

--- Economy_Simulator/Economy_Simulator.java
+++ Economy_Simulator/Economy_Simulator.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Economy_Simulator {
 
+	//Gets a random number from 0 to 9
 	public static int randomNumber() {
 		return ((int)((Math.random()) * 10));
-	}
-	
-	public static float economicGrowth() {
-		
-	}
-	
-	public static int researchTime() {
-		
 	}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		String selection;
+		String selection, growth;
+		int gdpA, gdpB, tradeValue, total;
+		float percentOfExport;
 		
 		System.out.println("Would you like the use Economy or Research Simulation? Y/N");
 		selection = scanner.nextLine();
 		if(selection == "Y") {
-			economicGrowth();
+			System.out.println("Do you want to calculate Growth from Trade or Building?");
+			growth = scanner.nextLine();
+			if(growth == "Trade") {
+				System.out.println("Please input the GDP of the country recieving the trade.");
+				gdpA = scanner.nextInt();
+				System.out.println("Please input the GDP of the country giving the trade.");
+				gdpB = scanner.nextInt();
+			}
 		}
 		else {
-			researchTime();
+			
 		}
 	}
 

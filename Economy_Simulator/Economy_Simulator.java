bad2f26cf497e57dd9b13ed1053f78bd5c7bd2c0
Andrew Knee <andrew.knee.j@gmail.com>
2018-06-12 22:49:22 -0400

    Update Economy_Simulator.java

--- Economy_Simulator/Economy_Simulator.java
+++ Economy_Simulator/Economy_Simulator.java
@@ -8,8 +8,27 @@ public class Economy_Simulator {
 		return ((int)((Math.random()) * 10));
 	}
 	
-	public static void main(String[] args) {
+	public static float economicGrowth() {
 		
+	}
+	
+	public static int researchTime() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String selection;
+		
+		System.out.println("Would you like the use Economy or Research Simulation? Y/N");
+		selection = scanner.nextLine();
+		if(selection == "Y") {
+			economicGrowth();
+		}
+		else {
+			researchTime();
+		}
 	}
 
 }

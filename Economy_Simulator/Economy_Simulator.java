6b466b40da0e2414844a461b3d29a15ccea2977b
Andrew Knee <andrew.knee.j@gmail.com>
2018-06-12 22:39:44 -0400

    Update Economy_Simulator.java

--- Economy_Simulator/Economy_Simulator.java
+++ Economy_Simulator/Economy_Simulator.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class Economy_Simulator {
 
+	public static int randomNumber() {
+		return ((int)((Math.random()) * 10));
+	}
+	
 	public static void main(String[] args) {
 		
-
 	}
 
 }
